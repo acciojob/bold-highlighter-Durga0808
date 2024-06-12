71769db5159175ec2f82528dd9cd0462ba7ac0aa
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-06-12 14:48:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
-let boldwords=document.getElementByTagName('strong');
+let boldwords=document.getElementsByTagName('strong');
 	for (let i = 0; i < boldwords.length; i++) {
 		boldwords[i].classList.remove('highlight');
 	}
