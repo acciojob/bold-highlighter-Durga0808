ed0cf050890a81cf6c9c4e478430edcceb49d18d
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-06-12 14:46:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	let boldwords=document.getElementsByTagName('strong');
+	for (let i = 0; i < boldwords.length; i++) {
+		boldwords[i].classList.add('highlight');;
+	}
 }
 
 
 function return_normal() {
     //Write your code here
-
+let boldwords=document.getElementByTagName('strong');
+	for (let i = 0; i < boldwords.length; i++) {
+		boldwords[i].classList.remove('highlight');
+	}
     
 }
